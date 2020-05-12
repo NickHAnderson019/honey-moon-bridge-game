bea72ac06d2a32a0753ce540e0e7f65535c76133
NickHAnderson019 <nick@thalana.co.za>
2020-05-12 10:19:39 +0200

    Removed all files

--- hmbGameApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HmbgameappConfig(AppConfig):
-    name = 'hmbGameApp'
--- hmbGameApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-app_name='hmbGameApp'
-
-urlpatterns = [
-    path('singleplayer/', views.game, name='game'),
-    path('multiplayer/', views.mpgame, name='mpgame'),
-]
